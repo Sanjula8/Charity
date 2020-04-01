ea155b9ee1eaa876a212e24d449aaa2bbff15ebd
Sanjula <sanjula8@gmail.com>
2020-03-31 19:11:11 -0500

    changed app.js with different routes, added search functionality for members page, added different componenets and pages for everything to render correctly

--- client/src/pages/Members.js
+++ client/src/pages/Members.js
@@ -1,14 +1,22 @@
 import React from "react";
+import MembersBar from "../components/MembersNav/index";
+import CharityCard from "../components/CharitySearch/index";
 // import './App.css';
 
 function Members() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Welcome <span className="member-name"></span>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<MembersBar />
+			<div className="container">
+				<div className="text-center">
+					<h1>Welcome USER</h1>
+				</div>
+				<div>
+					<CharityCard />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Members;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 // import About from "./pages/About";
 // import Search from "./pages/Members";
 // import Search from "./pages/Home";
 import Registration from "../components/Registration";
+import Navbar from "../components/Nav/index";
+import Footer from "../components/Footer/index";
+
 // import Footer from "./components/Footer";
 // import './App.css';
 
-class Register extends Component {
-  render (){
-
-    return(
-
-        <Registration/>
-
-    )}
+function Register() {
+	return (
+		<div>
+			<Navbar />
+			<Registration />
+			<Footer />
+		</div>
+	);
 }
 
 export default Register;
