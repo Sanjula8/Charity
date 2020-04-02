1d62fdd218413d93fa43f680ac808f2707d49502
Stefania Stefansdottir <sstefansdottir88@gmail.com>
2020-04-02 11:06:27 -0500

    made changes to navbar

--- client/src/components/CharitySearch/index.js
+++ client/src/components/CharitySearch/index.js
@@ -2,82 +2,71 @@ import React, { useState, useEffect } from "react";
 var axios = require("axios");
 
 function CharitySearch() {
-	const [data, setData] = useState([]);
-	const [search, setSearch] = useState("");
-	const [selectedCharity, setSelectedCharity] = useState({});
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [selectedCharity, setSelectedCharity] = useState({});
 
-	function handleSearchChange(event) {
-		const { value } = event.target;
-		setSearch(value);
-	}
+  function handleSearchChange(event) {
+    const { value } = event.target;
+    setSearch(value);
+  }
 
-	async function handleFormSubmit(event) {
-		event.preventDefault();
-		const result = await axios(`/api/charity/${search}`);
-		setData(result.data);
-	}
+  async function handleFormSubmit(event) {
+    event.preventDefault();
+    const result = await axios(`/api/charity/${search}`);
+    setData(result.data);
+  }
 
-	async function handleSelected(ein) {
-		const result = await axios(`/api/charity/id/${ein}`);
-		setSelectedCharity(result.data);
-	}
+  async function handleSelected(ein) {
+    const result = await axios(`/api/charity/id/${ein}`);
+    setSelectedCharity(result.data);
+  }
 
-	//     if () {
-
-	//     }
-	// } else {
-
-	// }
-
-	return (
-		<div>
-			<div className="container">
-				<div className="card">
-					<h5 className="card-header">Search for a Charity</h5>
-					<div className="card-body">
-						<h5 className="card-title">
-							Enter a cause you want to support!
-						</h5>
-						<form className="form-inline d-flex justify-content-center md-form form-sm mt-0">
-							<i className="fas fa-search" aria-hidden="true"></i>
-							<input
-								className="form-control form-control-sm ml-3 w-75"
-								type="text"
-								placeholder="Search"
-								aria-label="Search"
-								type="text"
-								name="search"
-								onChange={handleSearchChange}
-							/>
-							<button
-								className="btn btn-primary ml-2"
-								type="submit"
-								onClick={handleFormSubmit}
-							>
-								Search
-							</button>
-						</form>
-						<ul className="list-group col-6 pt-4">
-							{data.map(item => (
-								<li className="list-group-item">
-									<button
-										type="button"
-										className="btn btn-link"
-										onClick={event =>
-											handleSelected(item.ein)
-										}
-										data-ein={item.ein}
-									>
-										{item.charityName}
-									</button>
-								</li>
-							))}
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="container">
+        <div className="card">
+          <h5 className="card-header">Search for a Charity</h5>
+          <div className="card-body">
+            <h5 className="card-title">Enter a cause you want to support!</h5>
+            <form className="form-inline d-flex justify-content-center md-form form-sm mt-0">
+              <i className="fas fa-search" aria-hidden="true"></i>
+              <input
+                className="form-control form-control-sm ml-3 w-75"
+                type="text"
+                placeholder="Search"
+                aria-label="Search"
+                type="text"
+                name="search"
+                onChange={handleSearchChange}
+              />
+              <button
+                className="btn btn-primary ml-2"
+                type="submit"
+                onClick={handleFormSubmit}
+              >
+                Search
+              </button>
+            </form>
+            <ul className="list-group col-6 pt-4">
+              {data.map(item => (
+                <li className="list-group-item">
+                  <button
+                    type="button"
+                    className="btn btn-link"
+                    onClick={event => handleSelected(item.ein)}
+                    data-ein={item.ein}
+                  >
+                    {item.charityName}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CharitySearch;
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import twitterIcon from "../../images/twitter-icon.png";
+import faceIcon from "../../images/facebook-icon.png";
+import instaIcon from "../../images/insta-icon.png";
 import "./style.css";
 
 function Nav() {
   return (
-    <div className="container">
+    <>
+      <div className="row" style={{ height: "50px", backgroundColor: "white" }}>
+        <div className="social-icon">
+          <img src={twitterIcon} width="20" height="20" alt="twitter" />
+          <img src={faceIcon} width="20" height="25" alt="facebook" />
+          <img src={instaIcon} width="20" height="20" alt="instagram" />
+        </div>
+      </div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="/">
           Charity Hub
@@ -28,7 +38,7 @@ function Nav() {
           </ul>
         </div>
       </nav>
-    </div>
+    </>
   );
 }
 export default Nav;
