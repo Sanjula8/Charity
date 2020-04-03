452af3c538ebe45d661120660b6836aaf55a0c39
Stefania Stefansdottir <sstefansdottir88@gmail.com>
2020-04-03 09:04:16 -0500

    changed port number

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var session = require("express-session");
 var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3001;
 var db = require("./models");
 
 // Serve up static assets (usually on heroku)
