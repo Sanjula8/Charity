307607def665e3f4f811d9b9310b89c34af16563
Sanjula <sanjula8@gmail.com>
2020-04-07 09:34:24 -0500

    fixed api env thing

--- utils/charityAPI.js
+++ utils/charityAPI.js
@@ -1,16 +1,16 @@
 var axios = require("axios");
 
 function searchWithTerm(query) {
-  // Gets all Non-Profits Organizations
-  return axios.get(
-    `https://api.data.charitynavigator.org/v2/Organizations?app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}&search=${query}`
-  );
+	// Gets all Non-Profits Organizations
+	return axios.get(
+		`https://api.data.charitynavigator.org/v2/Organizations?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&search=${query}`
+	);
 }
 
 function searchWithEIN(ein) {
-  return axios.get(
-    `https://api.data.charitynavigator.org/v2/Organizations/${ein}?app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}`
-  );
+	return axios.get(
+		`https://api.data.charitynavigator.org/v2/Organizations/${ein}?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`
+	);
 }
 
 module.exports = { searchWithTerm, searchWithEIN };
