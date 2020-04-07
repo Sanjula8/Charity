099e4cad264d5af065d50edc16e7727efe33d36a
Stefania Stefansdottir <sstefansdottir88@gmail.com>
2020-04-07 08:27:47 -0500

    API and env update

--- utils/charityAPI.js
+++ utils/charityAPI.js
@@ -1,16 +1,16 @@
 var axios = require("axios");
 
 function searchWithTerm(query) {
-	// Gets all Non-Profits Organizations
-	return axios.get(
-		`https://api.data.charitynavigator.org/v2/Organizations?app_id=7c330aa5&app_key=e71ce963795487b79ed62c51b21c0529&search=${query}`
-	);
+  // Gets all Non-Profits Organizations
+  return axios.get(
+    `https://api.data.charitynavigator.org/v2/Organizations?app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}&search=${query}`
+  );
 }
 
 function searchWithEIN(ein) {
-	return axios.get(
-		`https://api.data.charitynavigator.org/v2/Organizations/${ein}?app_id=7c330aa5&app_key=e71ce963795487b79ed62c51b21c0529`
-	);
+  return axios.get(
+    `https://api.data.charitynavigator.org/v2/Organizations/${ein}?app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}`
+  );
 }
 
 module.exports = { searchWithTerm, searchWithEIN };
